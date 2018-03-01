ab1fac0bf99024be9f1637a2f82a9f192fc20277
temp <temp>
2018-03-01 12:24:17 -0500

    Add project files.

---
+++ variables/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace variables
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hockeyButton = new System.Windows.Forms.Button();
+            this.areaButton = new System.Windows.Forms.Button();
+            this.carpetButton = new System.Windows.Forms.Button();
+            this.billButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // hockeyButton
+            // 
+            this.hockeyButton.Location = new System.Drawing.Point(12, 21);
+            this.hockeyButton.Name = "hockeyButton";
+            this.hockeyButton.Size = new System.Drawing.Size(72, 37);
+            this.hockeyButton.TabIndex = 0;
+            this.hockeyButton.Text = "Hockey";
+            this.hockeyButton.UseVisualStyleBackColor = true;
+            this.hockeyButton.Click += new System.EventHandler(this.hockeyButton_Click);
+            // 
+            // areaButton
+            // 
+            this.areaButton.Location = new System.Drawing.Point(12, 79);
+            this.areaButton.Name = "areaButton";
+            this.areaButton.Size = new System.Drawing.Size(72, 37);
+            this.areaButton.TabIndex = 1;
+            this.areaButton.Text = "Area";
+            this.areaButton.UseVisualStyleBackColor = true;
+            this.areaButton.Click += new System.EventHandler(this.areaButton_Click);
+            // 
+            // carpetButton
+            // 
+            this.carpetButton.Location = new System.Drawing.Point(12, 145);
+            this.carpetButton.Name = "carpetButton";
+            this.carpetButton.Size = new System.Drawing.Size(72, 34);
+            this.carpetButton.TabIndex = 2;
+            this.carpetButton.Text = "Carpet";
+            this.carpetButton.UseVisualStyleBackColor = true;
+            this.carpetButton.Click += new System.EventHandler(this.carpetButton_Click);
+            // 
+            // billButton
+            // 
+            this.billButton.Location = new System.Drawing.Point(12, 204);
+            this.billButton.Name = "billButton";
+            this.billButton.Size = new System.Drawing.Size(72, 32);
+            this.billButton.TabIndex = 3;
+            this.billButton.Text = "Bill";
+            this.billButton.UseVisualStyleBackColor = true;
+            this.billButton.Click += new System.EventHandler(this.billButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.outputLabel.Location = new System.Drawing.Point(108, 21);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(211, 215);
+            this.outputLabel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(342, 261);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.billButton);
+            this.Controls.Add(this.carpetButton);
+            this.Controls.Add(this.areaButton);
+            this.Controls.Add(this.hockeyButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button hockeyButton;
+        private System.Windows.Forms.Button areaButton;
+        private System.Windows.Forms.Button carpetButton;
+        private System.Windows.Forms.Button billButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ variables/Form1.cs
@@ -0,0 +1,84 @@
+﻿//Feb 28 2018
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace variables
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hockeyButton_Click(object sender, EventArgs e)
+        {
+            string playerName = "Wayne Gretzky is #99";
+
+            outputLabel.Text = playerName;  // Displays Wayne Gretzky
+
+
+
+        }
+
+        private void areaButton_Click(object sender, EventArgs e)
+        {
+            double Pi, radius;
+            double product, area;
+            Pi = 3.14;
+            radius = 15;
+            product = Pi * radius;
+            area = product * product;
+
+            outputLabel.Text = "\nThe area of a circle is" + area + "cm^2";
+
+        }
+
+        private void carpetButton_Click(object sender, EventArgs e)
+        {
+            //8.5 m by 6 m if the carpet has a price of 19.95 per square meter. 
+            double area, price;
+            double side1, side2, totalArea;
+            double product;
+            side1 = 8.5;
+            side2 = 6;
+            totalArea = side1 * side2;
+            area = 51;
+            price = 19.95;
+            product = area * price;
+
+            outputLabel.Text = "\nThe area of the carpet is" + totalArea + "cm^2";
+
+            outputLabel.Text += "\nThe price of the carpet is" + product;
+            
+        }
+
+        private void billButton_Click(object sender, EventArgs e)
+        {
+            //• A shirt was purchased for $12.49 and was paid with a $20 bill.
+            //• Calculate taxes, (HST = 13 %), total bill, and change.
+            //Output the entire bill showing purchased price, taxes, total, and change.
+            double shirt, taxes, taxeincluded;
+            double bill, change, total;
+            shirt = 12.49;
+            bill = 20.00;
+            taxes = 0.13;
+            taxeincluded = shirt * taxes;
+            total = bill - taxeincluded;
+
+            outputLabel.Text = "The taxe on the shirt is" + taxeincluded;
+            outputLabel.Text = "\nThe total change due is " + total;
+
+
+
+        }
+    }
+}
